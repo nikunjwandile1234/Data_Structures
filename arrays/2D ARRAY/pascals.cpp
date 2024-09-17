12658d0ed62e199f2350b2ee0f021e75f329c2a5
nikunjwandile1234 <157633280+nikunjwandile1234@users.noreply.github.com>
2024-09-17 12:27:58 +0530

    pascal

---
+++ arrays/2D ARRAY/pascals.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<vector<int>> pascaltriangle(int &n)
+{
+    vector<vector<int>> pascal(n);
+    for(int i = 0; i < n; i++)
+    {
+        pascal[i].resize(i + 1);
+
+        for(int j = 0; j <= i; j++)  // corrected loop condition for i+1
+        {
+            if(j == 0 || j == i)
+            {
+                pascal[i][j] = 1;  
+            }
+            else
+            {
+                pascal[i][j] = pascal[i - 1][j - 1] + pascal[i - 1][j];  
+            }
+        }
+    }
+    return pascal;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of Pascal's triangle: " << endl;
+    cin >> n;
+
+    vector<vector<int>> ans = pascaltriangle(n);
+
+    for(int i = 0; i < ans.size(); i++)
+    {
+        for(int j = 0; j < ans[i].size(); j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
+
